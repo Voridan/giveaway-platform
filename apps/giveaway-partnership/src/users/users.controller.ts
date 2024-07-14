fd25d837441a85a38e8e03a8412a5815daac1ccc
Vorobienko Bohdan <bogdanvorobienko@gmail.com>
2024-07-14 16:14:52 +0300

    refactored user controller

--- apps/giveaway-partnership/src/users/users.controller.ts
+++ apps/giveaway-partnership/src/users/users.controller.ts
@@ -1,30 +1,19 @@
 import {
-  Body,
   Controller,
-  Delete,
   Get,
   NotFoundException,
   Param,
-  Patch,
   Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDto } from './dto/user.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { CurrentUser } from '../decorators';
 
 @Controller('users')
 @Serialize(UserDto)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Get()
-  // @Query('limit') limit: number, @Query('offset') offset: number
-  findAllUsers() {
-    // console.log(`limit: ${limit}, offset: ${offset}`);
-    return this.usersService.findAll();
-  }
 
   @Get('/search')
   async findSimilar(
@@ -45,15 +34,5 @@ export class UsersController {
     }
 
     return user;
-  }
-
-  @Patch('/:id')
-  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-    return this.usersService.update(parseInt(id), body);
-  }
-
-  @Delete('/:id')
-  removeUser(@Param('id') id: string) {
-    return this.usersService.remove(parseInt(id));
   }
 }
