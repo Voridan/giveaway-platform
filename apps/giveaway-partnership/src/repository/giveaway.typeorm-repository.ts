65416076ec8ad67941d98a84694d3f4ec70bd4ab
Vorobienko Bohdan <bogdanvorobienko@gmail.com>
2024-07-28 15:52:55 +0300

    add last item id pagination

--- apps/giveaway-partnership/src/repository/giveaway.typeorm-repository.ts
+++ apps/giveaway-partnership/src/repository/giveaway.typeorm-repository.ts
@@ -36,7 +36,13 @@ export class GiveawayTypeOrmRepository extends GenericTypeOrmRepository<Giveaway
     return qb.take(limit).orderBy('giveaway.id').getManyAndCount();
   }
 
-  getPartneredGiveaways(partnerId: number, offset: number, limit: number) {
+  getPartneredGiveaways(
+    partnerId: number,
+    offset: number,
+    limit: number,
+    lastItemId: number,
+    forward: boolean = true,
+  ) {
     const qb = this.entityRepository
       .createQueryBuilder('giveaway')
       .innerJoinAndSelect(
@@ -47,14 +53,38 @@ export class GiveawayTypeOrmRepository extends GenericTypeOrmRepository<Giveaway
       )
       .where('gpu.giveawayId = giveaway.id');
 
-    return qb.skip(offset).take(limit).orderBy('giveaway.id').getManyAndCount();
+    if (lastItemId) {
+      if (forward) {
+        qb.andWhere('giveaway.id > :lastItemId', { lastItemId });
+      } else {
+        qb.andWhere('giveaway.id < :lastItemId', { lastItemId });
+      }
+    }
+    qb.orderBy('giveaway.id', forward ? 'ASC' : 'DESC');
+    qb.skip(offset);
+    qb.take(limit);
+    return qb.getManyAndCount();
   }
 
-  getOwnGiveaways(ownerId: number, offset: number, limit: number) {
+  getOwnGiveaways(
+    ownerId: number,
+    offset: number,
+    limit: number,
+    lastItemId: number,
+    forward: boolean = true,
+  ) {
     const qb = this.entityRepository.createQueryBuilder('giveaway');
     qb.where('giveaway.ownerId = :ownerId', { ownerId });
+    if (lastItemId) {
+      if (forward) {
+        qb.andWhere('giveaway.id > :lastItemId', { lastItemId });
+      } else {
+        qb.andWhere('giveaway.id < :lastItemId', { lastItemId });
+      }
+    }
+    qb.orderBy('giveaway.id', forward ? 'ASC' : 'DESC');
     qb.skip(offset);
-
-    return qb.take(limit).orderBy('giveaway.id').getManyAndCount();
+    qb.take(limit);
+    return qb.getManyAndCount();
   }
 }
